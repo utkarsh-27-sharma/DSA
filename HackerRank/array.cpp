6782a8e85a8b43af66dadaf552a707187c1be18d
Utkarsh Sharma <65302150+utkarsh-27-sharma@users.noreply.github.com>
2021-06-14 17:47:29 +0530

    Create array.cpp

---
+++ HackerRank/array.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+            int N;
+            cin>>N;
+    
+            vector<int> arr(N);
+    
+            for (int i=0;i<N;i++)
+                cin>>arr[i];
+    
+            reverse(arr.begin(),arr.end());
+    
+                for (int i=0;i<N;i++)
+                cout<<arr[i]<<" ";
+    
+            return 0;
+}
+
+// https://www.hackerrank.com/challenges/arrays-ds/problem
