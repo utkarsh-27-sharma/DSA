6c39676220fb378a2a4a2d7de1bd8be7e77b907a
Utkarsh Sharma <65302150+utkarsh-27-sharma@users.noreply.github.com>
2021-06-19 00:19:04 +0530

    Create magic.cpp

---
+++ GeeksForGeeks/magic.cpp
@@ -0,0 +1,22 @@
+// Function to swap A and B
+// using pointers
+void swap(int *A, int *B){
+    
+    // Your code here
+    int temp=*A;
+    *A=*B;
+    *B=temp;
+    
+}
+
+// Function to swap A and B
+// using reference
+void swap(int &A, int &B){
+    
+    int temp=A;
+    A=B;
+    B=temp;
+  
+    // Your code here
+  
+}
