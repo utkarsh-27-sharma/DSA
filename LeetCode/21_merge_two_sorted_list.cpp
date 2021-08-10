31b1ef1990acb1a1e284fb74d492956b9a0b3d02
Utkarsh Sharma <65302150+utkarsh-27-sharma@users.noreply.github.com>
2021-08-10 20:19:57 +0530

    initial commit

---
+++ LeetCode/21_merge_two_sorted_list.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1 == NULL) return l2;
+        if(l2 == NULL) return l1;
+        if(l1-> val > l2 -> val) std :: swap(l1,l2);
+        ListNode* res = l1;
+        while(l1 != NULL && l2 != NULL){
+            ListNode * tmp = NULL;
+            while (l1 != NULL && l1 -> val <= l2 ->val){
+                tmp = l1;
+                l1 = l1->next;
+            }
+            tmp->next = l2;
+            std::swap(l1,l2);
+        }
+        return res;
+    }
+};
+// https://leetcode.com/problems/merge-two-sorted-lists/
